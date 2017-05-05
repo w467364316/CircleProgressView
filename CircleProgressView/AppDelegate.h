4792c138cee96d164020005d765351eff76a7f7b
online 潇潇 <Jianjian@jianjian-macdemac-mini.local>
2017-05-05 14:57:30 +0800

    Initial Commit

---
+++ CircleProgressView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CircleProgressView
+//
+//  Created by JianJian-Mac on 17/5/5.
+//  Copyright © 2017年 WangJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CircleProgressView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CircleProgressView
+//
+//  Created by JianJian-Mac on 17/5/5.
+//  Copyright © 2017年 WangJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
